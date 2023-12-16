004664517900c0bfe7abb15e3db279f6386e105d
jungyunmok <jungyunmok@gmail.com>
2023-12-16 16:22:27 +0900

    feat: 월과 시작요일 입력하면 달력 반환하는 컨트롤러

--- src/main/java/oncall/controller/MainController.java
+++ src/main/java/oncall/controller/MainController.java
@@ -1,4 +1,39 @@
 package oncall.controller;
 
+import oncall.model.Exception;
+import oncall.model.MonthMaker;
+import oncall.view.InputVIew;
+import oncall.view.OutputVIew;
+
+import java.util.List;
+import java.util.Map;
+
 public class MainController {
+    InputVIew inputVIew = new InputVIew();
+    OutputVIew outputVIew = new OutputVIew();
+    Exception exception = new Exception();
+    MonthMaker monthMaker = new MonthMaker();
+
+
+    // 시작
+    public void start() {
+
+    }
+
+    // 월과 요일 입력하고 달력 반환하기
+    private List<String> fixMonth() {
+        List<String> monthInfo = null;
+        try {
+            String monthDay = inputVIew.readMonthDay();
+            Map<Integer, String> startInfo = exception.checkMonthDay(monthDay);
+            monthInfo = monthMaker.returnMonth(startInfo);
+            monthInfo = monthMaker.addHoliday(monthInfo);
+             return monthInfo;
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요.");
+            fixMonth();
+        }
+        return monthInfo;
+    }
+
 }
