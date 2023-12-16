efee463c8fc69660509ad2ece3b855d00cbb6598
jungyunmok <jungyunmok@gmail.com>
2023-12-16 16:13:09 +0900

    feat: 평일, 휴일 근무 사원 입력 기능

--- src/main/java/oncall/view/InputVIew.java
+++ src/main/java/oncall/view/InputVIew.java
@@ -10,4 +10,17 @@ public class InputVIew {
         return input;
     }
 
+    // 평일 근무 사원 입력받기
+    public String readWeekday() {
+        System.out.println("평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
+        String input = Console.readLine();
+        return input;
+    }
+
+    // 휴일 근무 사원 입력받기
+    public String readWeekend() {
+        System.out.println("휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
+        String input = Console.readLine();
+        return input;
+    }
 }
