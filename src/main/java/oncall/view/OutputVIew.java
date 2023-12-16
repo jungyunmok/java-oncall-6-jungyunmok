60640f0a97b124aa11ab5ba8bd9fb71ae851dd20
jungyunmok <jungyunmok@gmail.com>
2023-12-16 16:15:00 +0900

    feat: 비상 근무표 출력 기능

--- src/main/java/oncall/view/OutputVIew.java
+++ src/main/java/oncall/view/OutputVIew.java
@@ -1,4 +1,12 @@
 package oncall.view;
 
+import java.util.List;
+
 public class OutputVIew {
+    // 비상 근무표 출력
+    public void printSchedule(List<String> scheduleResult) {
+        for(String schedule : scheduleResult) {
+            System.out.println(schedule);
+        }
+    }
 }
