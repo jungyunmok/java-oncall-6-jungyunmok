38f7a6dc4ebb03fa339cc8c37d5de2b7dba3463c
jungyunmok <jungyunmok@gmail.com>
2023-12-16 16:12:06 +0900

    feat: 배정할 월과 시작 요일 입력 기능

--- src/main/java/oncall/view/InputVIew.java
+++ src/main/java/oncall/view/InputVIew.java
@@ -1,4 +1,13 @@
 package oncall.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputVIew {
+    // 배정할 월과 시작 요일 받기
+    public String readMonthDay() {
+        System.out.println("비상 근무를 배정할 월과 시작 요일을 입력하세요> ");
+        String input = Console.readLine();
+        return input;
+    }
+
 }
