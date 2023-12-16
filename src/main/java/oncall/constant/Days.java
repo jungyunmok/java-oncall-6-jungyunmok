8a572772328fa8fa66537f9fb5b6893db7d4222a
jungyunmok <jungyunmok@gmail.com>
2023-12-16 14:58:32 +0900

    feat: 해당 월과 시작 요일의 달력 만드는 기능

--- src/main/java/oncall/constant/Days.java
+++ src/main/java/oncall/constant/Days.java
@@ -1,6 +1,29 @@
 package oncall.constant;
 
-public class Days {
-    // 총 요일
-    public static final String[] DAYS = {"월", "화", "수", "목", "금", "토", "일"};
+import java.util.List;
+
+public enum Days {
+    D1(0, "월"),
+    D2(1, "화"),
+    D3(2, "수"),
+    D4(3, "목"),
+    D5(4, "금"),
+    D6(5, "토"),
+    D7(6, "일");
+
+    private final int DAYS_INDEX;
+    private final String DAYS_NAME;
+
+    Days(int DAYS_INDEX, String DAYS_NAME) {
+        this.DAYS_INDEX = DAYS_INDEX;
+        this.DAYS_NAME = DAYS_NAME;
+    }
+
+    public int getDAYS_INDEX() {
+        return DAYS_INDEX;
+    }
+
+    public String getDAYS_NAME() {
+        return DAYS_NAME;
+    }
 }
--- src/main/java/oncall/model/MonthMaker.java
+++ src/main/java/oncall/model/MonthMaker.java
@@ -1,5 +1,6 @@
 package oncall.model;
 
+import oncall.constant.Days;
 import oncall.constant.Month;
 
 import java.util.ArrayList;
@@ -11,26 +12,69 @@ public class MonthMaker {
     public List<String> returnMonth(Map<Integer, String> startInfo) {
         int startMonth = 0;
         String startDay = "";
-        for(Integer key : startInfo.keySet()) {
+        for (Integer key : startInfo.keySet()) {
             startMonth = key;
             startDay = startInfo.get(key);
         }
         int dayCount = returnDayCount(startMonth);
-
-        List<String> monthInfo = makeMonth(dayCount, startMonth, startDay);
+        List<String> monthInfo = makeMonth(startMonth, dayCount, startDay);
         return monthInfo;
     }
 
     // 해당 월에 맞는 일수 반환
     private int returnDayCount(int startMonth) {
-        List<String> monthInfo = new ArrayList<>();
         int dayCount = 0;
-        for(Month month : Month.values()) {
-            if(month.getMONTHLIST().contains(startMonth)) {
+        for (Month month : Month.values()) {
+            if (month.getMONTHLIST().contains(startMonth)) {
                 dayCount = month.getDAYS_COUNT();
             }
         }
         return dayCount;
     }
 
+    // 달력 만들어 반환하기
+    private List<String> makeMonth(int startMonth, int dayCount, String startDay) {
+        int index = findIndex(startDay);
+        List<String> monthInfo = repeatInsert(startMonth, dayCount, index);
+        return monthInfo;
+    }
+
+    // 맞는 시작 요일 인덱스 찾기
+    private int findIndex(String startDay) {
+        int index = 0;
+        for (Days days : Days.values()) {
+            if (startDay.equals(days.getDAYS_INDEX())) {
+                index = days.getDAYS_INDEX();
+                return index;
+            }
+        }
+        return index;
+    }
+
+    // 인덱스로 반복해서 날짜 넣기
+    private List<String> repeatInsert(int startMonth, int dayCount, int index) {
+        List<String> monthInfo = new ArrayList<>();
+        int date = 0;
+        while (date < dayCount) {
+            date = addInfo(monthInfo, index, startMonth);
+        }
+        return monthInfo;
+    }
+
+    // 조건 판단하고 정보 넣어서 리턴하기
+    private int addInfo(List<String> monthInfo,int index, int startMonth) {
+        int date = 1;
+        for (Days days : Days.values()) {
+            if (index == days.getDAYS_INDEX()) {
+                monthInfo.add(startMonth + "월 " + date + "일 " + days.getDAYS_NAME());
+                date++;
+            }
+            if (index >= 6) {
+                index = 0;
+            }
+            index++;
+
+        }
+        return date;
+    }
 }
